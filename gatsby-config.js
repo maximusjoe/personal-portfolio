68b3e676a04c8fd8012d257e1920281f50b055bf
Maximus Joe <maximusjoe278@gmail.com>
2022-04-26 10:49:20 -0700

    favicon changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,11 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'REPLACE_ME REPLACE_ME',
+    title: 'Maximus Joe',
     description:
       'REPLACE_ME REPLACE_ME is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://REPLACE_MEREPLACE_ME.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@REPLACE_ME',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
